661fcfca837b7cf4c8fd957fcc27a918b88dc9b5
0ddbird <giacomo.llinares@gmail.com>
2022-12-02 07:50:10 +0100

    Added transitions and basic carousel features

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,33 @@
+const navIconGreen = "<img class='arrow-green' src='../../assets/arrow.svg'/>"
+const navIconWhite = "<img class='arrow-white' src='../../assets/arrow-white.svg'/>"
+
+const navText = [`<div class='nav-button prev owl-prev'>${navIconGreen}${navIconWhite}</div>`, `<div class='nav-button next owl-next'>${navIconGreen}${navIconWhite}</div>`]
+
+
 $(document).ready(function(){
-  $(".owl-carousel").owlCarousel({
-    margin:20,
-    nav:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:2
-        },
-        1000:{
-            items:3
+  $(".owl-carousel").owlCarousel(
+    {
+        nav: true,
+        loop: true,
+        margin: 5,
+        stagePadding: 30,
+        dots: false,
+        center: false,
+        navText,
+        responsive: {
+            0: {
+                nav: true,
+                items: 1,
+                loop: true,
+            },
+            600: {
+                nav: true,
+                items: 2
+            },
+            1000: {
+                nav: true,
+                items: 3
+            }
         }
-    }
-});
+    });
 });
